ea523ee757b562d9aeb0159e1803c9344ffe8208
Ilya Shtandenko <i.shtandenko@ndmsystems.com>
2026-03-24 13:51:14 +0300

    SYS-1729 isc-dhcp: fix unexpected stop of Ip6::Dhcp::Server-Bridge0

--- common/tables.c
+++ common/tables.c
@@ -658,6 +658,7 @@ struct enumeration_value dhcpv6_message_values[] = {
 	{ "RECONFIGURE-REPLY", 19 },
 	{ "DHCPV4-QUERY", 20 },
 	{ "DHCPV4-RESPONSE", 21 },
+	{ "ADDR-REG-INFORM", 36 },
 	{ NULL, 0 }
 };
 
@@ -684,7 +685,22 @@ const char *dhcpv6_type_names[] = {
 	"Reconfigure-request",
 	"Reconfigure-reply",
 	"Dhcpv4-query",
-	"Dhcpv4-response"
+	"Dhcpv4-response",
+	NULL, /* 22 */
+	NULL, /* 23 */
+	NULL, /* 24 */
+	NULL, /* 25 */
+	NULL, /* 26 */
+	NULL, /* 27 */
+	NULL, /* 28 */
+	NULL, /* 29 */
+	NULL, /* 30 */
+	NULL, /* 31 */
+	NULL, /* 32 */
+	NULL, /* 33 */
+	NULL, /* 34 */
+	NULL, /* 35 */
+	"Addr-reg-inform" /* 36 */
 };
 const int dhcpv6_type_name_max =
 	(sizeof(dhcpv6_type_names) / sizeof(dhcpv6_type_names[0]));
